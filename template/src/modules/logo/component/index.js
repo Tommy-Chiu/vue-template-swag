d710284b12a7c53ac1534d987b7d57cd83840b00
Tommy Chiu <tommy.chiu.1024@gmail.com>
2020-11-16 22:30:48 +0800

    del（pages、modules）： del files

--- template/src/modules/logo/component/index.js
+++
@@ -1,3 +0,0 @@
-import { repuireAllAsJson } from '@/utils'
-const files = require.context('./', true, /\.vue$/)
-export default repuireAllAsJson(files)
--- template/src/pages/notFound/directive/index.js
+++
@@ -1,3 +0,0 @@
-import { repuireAllAsJson } from '@/utils'
-const files = require.context('./', true, /\.js$/)
-export default repuireAllAsJson(files)
